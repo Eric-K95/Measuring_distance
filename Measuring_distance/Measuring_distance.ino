afcfdc7ceba34b8223587b13be96f20c432a01d3
JINWOO KIM <73664547+Eric-K95@users.noreply.github.com>
2020-12-11 19:48:41 -0500

    Update Measuring_distance.ino

--- Measuring_distance/Measuring_distance.ino
+++ Measuring_distance/Measuring_distance.ino
@@ -28,15 +28,15 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.begin(20, 4);
-  lcd.setCursor(5, 0); //first column, row #5
+  lcd.setCursor(5, 0); //fifth column, row #1
   lcd.print("Hello!");
-  lcd.setCursor(0, 1); //second column, row #1
+  lcd.setCursor(0, 1); //first column, row #2
   lcd.print("Mechatronics:)");
   delay(2000);   //delay of the message
   lcd.clear();   // clear screen
   lcd.setCursor(0, 0); //first column, row #1
   lcd.print("UltrasonicSensor");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 1); //first column, row #2
   lcd.print(" Temp  &  Humid ");
   delay(2000); //delay 2000ms
   lcd.clear();
@@ -63,21 +63,21 @@ void loop() {
   //print values on lcd screen
   lcd.setCursor(0, 0); //first column, row #1
   lcd.print("Distance:"); //print name on LCD
-  lcd.setCursor(13, 0); //first column, row #13
+  lcd.setCursor(13, 0); //first column, row #2
   lcd.print("cm"); //print units
-  lcd.setCursor(10, 0); //first column, row #10
+  lcd.setCursor(10, 0); //tenth column, row #1
   lcd.print(dist); //print distance on LCD
-  lcd.setCursor(0, 1); //second coulumn, row#0
+  lcd.setCursor(0, 1); //first coulumn, row#2
   lcd.print("Temp:");
-  lcd.setCursor(5, 1); //second coulumn, row#5
+  lcd.setCursor(5, 1); //fifth coulumn, row#2
   lcd.print(tempC);
-  lcd.setCursor(7, 1); //second coulumn, row#7
+  lcd.setCursor(7, 1); //seventh coulumn, row#2
   lcd.print("c");  //unit of temperature
-  lcd.setCursor(8, 1); //second coulumn, row#8
+  lcd.setCursor(8, 1); //eight coulumn, row#2
   lcd.print("Humi:");
-  lcd.setCursor(13, 1); //second coulumn, row#13
+  lcd.setCursor(13, 1); //thirteenth coulumn, row#2
   lcd.print(humi);
-  lcd.setCursor(15, 1); //second coulumn, row#15
+  lcd.setCursor(15, 1); //fifteenth coulumn, row#2
   lcd.print("%"); //percentage of huminity
   delay(1000); //delay 1000ms
   lcd.clear(); //clear the LCD screen to show next measurement
